239286ff8b6bf62173962c6bdaa6e595f097381c
SandoDirga <81424237+SandoDirga@users.noreply.github.com>
2021-03-27 09:27:44 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+
+
+
+
+
+
+
+
+route::get('page/{nomor}', function($nomor){
+	return 'Ini Halaman ke-' . $nomor;
 });
